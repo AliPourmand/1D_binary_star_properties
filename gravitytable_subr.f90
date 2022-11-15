c3fab578c56b6976dedb381789ee0a290d10e0b1
Ali Pourmand <61276279+AliPourmand@users.noreply.github.com>
2022-11-15 13:50:36 -0700

    Add files via upload

---
+++ gravitytable_subr.f90
@@ -0,0 +1,399 @@
+!!! #######################################################################
+!!! ###  reading pre-calacuated table with effective acceleration       ###
+!!! ###  by default, the files are assumed to be in ./binary directory  ###
+!!! #######################################################################      
+
+      subroutine gravity_table_ini(M1,M2,r_lin_val,r_lout_val)
+
+         implicit none
+        
+         include "gravitytable.inc"        
+        
+!!! internal variables                 
+         integer :: j,l
+         integer :: n_q  !! this is the number of table that is read
+        
+         real*8 :: lgq_table(c_gtable)
+
+         character*255 :: file_name
+                        
+!!! dummy properties are those getting read from the tables        
+         real*8 :: xi_arr_dummy
+         real*8 :: A_shell_dummy, g_Lplane_arr_dummy
+         real*8 :: q_dummy(c_gtable),r_l2(c_gtable)
+         real*8 :: r_l3(c_gtable),a_lplane2(c_gtable)
+         real*8 :: a_lplane3(c_gtable), g_lplane2(c_gtable), g_lplane3(c_gtable)
+!!! inputs
+         real*8 :: M1, M2, q_input
+!!! output
+         real*8 :: r_lin_val, r_lout_val
+
+        
+         flag_gtable_ini=0
+
+         q_input = M1/M2   
+         write(*,*) "q_tot_input=",q_input
+         !!size gives length of array
+         n_q=c_gtable
+
+         do j=1,16
+
+            lgq_table(j)=-6.d0+(j-1)*0.25d0
+            q_table(j)=10.d0**lgq_table(j)
+
+         end do
+
+         do j=17,96
+
+            lgq_table(j)=-2.d0+(j-16-1)*0.05d0
+            q_table(j)=10.d0**lgq_table(j)
+
+         end do
+
+         do j=97,n_q
+
+            lgq_table(j)=2.d0+(j-96-1)*0.25d0
+            q_table(j)=10.d0**lgq_table(j)
+
+         end do        
+    
+
+         do j=1,n_q
+            if (lgq_table(j).lt.0) then 
+               write(file_name,'(A49,F6.3,A4)') "binary_tables_compact_ss_units/binary_props_logq="&
+               ,lgq_table(j),".dat"
+            end if
+            if (lgq_table(j).ge.-0.001d0) then 
+               write(file_name,'(A49,F5.3,A4)') "binary_tables_compact_ss_units/binary_props_logq="&
+               ,lgq_table(j),".dat"
+            end if
+
+            ! write (*,*) file_name
+           
+            open (unit = 23, file = file_name)
+
+            do l=1,n_gtable
+
+!! The complete format is as below. To find effective acceleration we only need to store in memory two columns, 5 and 6.
+!! Extra information is provided for MT calcualtions.
+!! 1 column: mass ratio of system (donor to companion)
+!! 2 column: volume equivalent radius of shell divided by eggleton radius
+!! 3 column: relative potential as defined in mochnacki  eg, for potentials higher than psiL1: F=psiL1/psi
+!! 4 column: potential of shell
+!! 5 column: volume equivalent radius of shell divided by orbital separation
+!! 6 column: avg acceleration on shell. 
+!! 7 column: total area of shell
+!! 7 column: avg acceleration on L plane cross section with shell
+!! 8 column: area of L plane cross section with shell
+!! 9 column: semi major axis of L plane cross section with shell
+!! 10 column: semi minor axis of L plane cross section with shell
+            
+               read (23,*) table_rel_r(j,l), xi_arr_dummy, table_grav(j,l),&
+                   A_shell_dummy, g_Lplane_arr_dummy
+            end do
+            close (23)
+
+
+         end do
+ 
+
+!!!!!!!!!!! reading the lagrange point properties from the file l1l2l3_props.dat
+
+ 
+
+         write(file_name,'(A16)') "l1l2l3_props.dat"
+           
+         open (unit = 24, file = file_name)
+
+         do j=1,n_q
+
+! The file l1l2l3_props.dat includes properties of the three lagrange points for these 109 mass ratios. The properties are listed below:
+
+! column 1: mass ratio of system (donor to companion)  
+! column 2: volume equivalent radius of shell passing L1  
+! column 3: volume equivalent radius of shell passing L2  
+! column 4: volume equivalent radius of shell passing L3  
+! column 5: area of L plane cross section with shell passing L2  
+! column 6: area of L plane cross section with shell passing L3  
+! column 7: avg acceleration on L plane cross section with shell passing L2  
+! column 8: avg acceleration on L plane cross section with shell passing L3     
+            
+            read (24,*) q_dummy(j),r_l1(j),r_l2(j),r_l3(j),a_lplane2(j),&
+                        a_lplane3(j), g_lplane2(j), g_lplane3(j)
+
+
+!!!!!!!!!!!! outer lagrange point is l2 for q <= 1
+            if (q_dummy(j).le.0.999d0) then
+               r_lout(j) = r_l2(j)
+            else
+               r_lout(j) = r_l3(j)
+            end if
+
+            ! write (*,*) r_lout(j)
+
+
+         end do
+
+! find input mass ratio's corresponding rlin and rlout   
+         
+         do j =1,n_q-1
+            if (q_input.ge.q_table(j).and.&
+                  q_input.lt.q_table(j+1)) then
+               
+               r_lin_val = linear_interpolant(q_table(j),q_table(j+1),&
+                  r_l1(j),r_l1(j+1),q_input)
+
+               r_lout_val = linear_interpolant(q_table(j),q_table(j+1),&
+                  r_lout(j),r_lout(j+1),q_input)
+
+               write(*,*) "called for mass ratio =",q_input
+               ! write(*,*) "inner lagrange volume equivalent radius =",r_lin_val
+               ! write(*,*) "1. outer lagrange vol radius =",r_lout_val
+
+               exit
+            end if
+         end do        
+
+
+
+
+         close (24)
+
+
+
+        flag_gtable_ini=1
+
+      end subroutine gravity_table_ini
+    
+
+
+!!! ##################################################################################
+!!! ###  uses pre-calacuated and pre-read tables with effective acceleration       ###
+!!! ###  input is in solar units, returns effective acceleration                   ###      
+!!! ###  M1 is the total mass (in Msun) of the star for which we find acceleration ###
+!!! ###  M2 is the total mass (in Msun) of the companion star                      ###
+!!! ###  a is the orbital separation (in Rsun)                                     ###
+!!! ###  r_loc is radial coordinate (in Rsun) inside the "donor" star in 1D code   ###
+!!! ###  M_loc is the mass coordinate inside the "donor" star (in Msun)            ###
+!!! ###  flag_accel: 1: the function returns effective acceleration  in CGS units  ###
+!!! ###              2 : returns in units of of "single star" acceleration         ###
+!!! ##################################################################################      
+
+
+      real*8  function effective_gravity(M1,M2,a,r_loc,M_loc, flag_accel)
+  
+         implicit none
+  
+         include "gravitytable.inc"        
+        
+!!! input and output variable
+         real*8 :: M1, M2, M_loc
+         real*8 :: r_loc, a
+         integer flag_accel
+           
+!!! locally used variables
+
+         real*8 grav_final
+         real*8 :: q_local, q_total, r_rel_loc
+          
+         real*8 :: grav_found(2), r_rel_thresh(2)
+          
+         integer :: j,l,k, flag_use_analyt
+         integer :: n_q, j_found, found_index, l_found(2)
+ 
+         real*8 :: table_f, g_sing, g_exterior
+         real*8 :: r_lin_val, r_lout_val
+
+ 
+         n_q=size(q_table)
+
+      
+         !!scaling the input
+         r_rel_loc=r_loc/a
+         ! write(*,*)"r_rel_loc", r_rel_loc
+         q_local=M_loc/M2
+         q_total=M1/M2  
+         ! write(*,*) "qlocal", q_local
+
+! find input mass ratio's corresponding rlin and rlout   
+
+         do j =1,n_q-1
+            if (q_total.ge.q_table(j).and.&
+                  q_total.lt.q_table(j+1)) then
+               
+               found_index = j
+               r_lin_val = linear_interpolant(q_table(j),q_table(j+1),&
+                  r_l1(j),r_l1(j+1),q_total)
+
+               r_lout_val = linear_interpolant(q_table(j),q_table(j+1),&
+                  r_lout(j),r_lout(j+1),q_total)
+                  
+               ! write(*,*) "found_index", found_index
+               ! write(*,*) "called for mass ratio =",q_total
+               ! write(*,*) "inner lagrange volume equivalent radius =",r_lin_val
+               ! write(*,*) "rloc =", r_loc
+               ! write(*,*) "outer lagrange volume equivalent radius =",r_lout_val
+               ! write(*,*) "m1/m2 =",(M1/M2)
+               ! write(*,*) "rloc/rout**3 =",(r_rel_loc/r_lout_val)**3.d0
+
+               ! write(*,*) "density limit qloc =",(M1/M2)*(r_rel_loc/r_lout_val)**3.d0
+               exit
+            end if
+         end do  
+
+         flag_use_analyt=0
+
+         
+         !! Check if the tables are initialized and called for acceptable parameters
+         
+         if(flag_gtable_ini.ne.1) then
+            write(*,*) "error, tables for effective gravity were not read"
+            stop
+         end if
+         
+         if(flag_accel.ne.1.and.flag_accel.ne.2) then
+            write(*,*) flag_accel, "error, accelerations can be requested as 1 (in CGS units) or 2 (in relative units)"
+            stop
+         end if
+         
+         if (M_loc.le.0.d0.or.M_loc.gt.M1) then
+            write (*,*) "error, local mass coordinate should be 0<M_local<=M1 donor star mass"
+            stop
+         end if
+         
+         if (r_loc.le.0.d0.or.r_loc.ge.a) then
+            write (*,*) "error, radius of zone should be  0<=r<=a (orbital separation)", r_loc, a 
+            stop
+         end if
+
+
+!!!!!!!!!!!!!! the following equations are from the article, equation 23
+
+         if(flag_accel.eq.1) then
+            effective_gravity = GT_accel*(1.d0+(-2.d0*(1.d0+&
+               q_total)*(r_rel_loc**3.d0)/(3.d0*q_local)))*M_loc/r_loc**2.d0
+         else
+            effective_gravity = (1.d0+(-2.d0*(1.d0+&
+               q_total)*(r_rel_loc**3.d0)/(3.d0*q_local)))*q_local/q_total
+
+         end if
+
+         if (q_total.gt.100000.d0) then
+            write (*,*) "error, the acceptable mass ratio q=M1/M2 is 1e-6<=q_total<=1e5"
+            stop
+         end if
+
+         if (q_total.lt. q_table(1)) then
+!! return pre-assigned value as in a single star             
+            write (*,*) "error, the acceptable mass ratio q=M1/M2 is 1e-6<=q_total<=1e5"
+            stop
+         end if
+
+
+          !! Searching for the two mass ratio tables to interpolate 
+    
+         j_found =-1
+
+         do j =1,n_q-1
+            if (q_total.ge.q_table(j).and.&
+                  q_total.lt.q_table(j+1)) then
+               j_found=j
+               ! write (*,*) "q_total=", q_total
+               ! write (*,*) "q_table(j)=", q_table(j)
+               ! write (*,*) "q_table(j+1)=", q_table(j+1)
+
+               exit
+            end if
+         end do
+
+
+
+!!!!!!!!!!!!! uses analytical solution for values smaller than what is in the tables
+
+         if (r_rel_loc.le.table_rel_r(j_found,1).or.&
+            r_rel_loc.le.table_rel_r(j_found+1,1)) then
+!! return pre-assigned value as in a single star     
+            flag_use_analyt =1 
+            write (*,*)"return"
+            return
+         end if 
+         
+          
+         if(j_found.eq.-1) then
+            write(*,*) j_found, q_total, "error, can not locate the table for this mass ratio"
+         end if
+
+         if (r_rel_loc.gt.r_lout_val.or.abs(r_rel_loc-r_lout_val).le.0.000001d0) then
+            write (*,*)" Stars exceeds L3 equivalent volume, check input", r_rel_loc !, table_rel_r(j_found,n_gtable) r_lout_val
+            stop
+         end if
+          
+
+
+         grav_found(1)=-1.
+         grav_found(2)=-1. 
+         do k=1,2  ! k is 1 and 2             
+            do l=1,(n_gtable-1)                
+               if (r_rel_loc.gt.&
+                     table_rel_r(k+j_found-1,l).and.&
+                     r_rel_loc.lt.&
+                     table_rel_r(k+j_found-1,l+1)) then
+
+                  l_found(k) = l
+                  grav_found(k) = linear_interpolant(table_rel_r(k+j_found-1,l),&
+                        table_rel_r(k+j_found-1,l+1),table_grav(k+j_found-1,l),&
+                        table_grav(k+j_found-1,l+1),r_rel_loc)
+                   
+                  exit                   
+               end if
+            end do
+         end do
+
+         if(grav_found(1).lt.0.or.grav_found(2).lt.0) then
+             write(*,*) "did not find appropriate radius in the table", r_rel_loc, &
+             table_rel_r(j_found,600),&
+             l_found(1),&
+             table_rel_r(j_found+1,l_found(2)),&
+             l_found(2),&
+              q_total
+             stop
+         end if
+
+
+         grav_final = linear_interpolant(q_table(j_found),&
+               q_table(j_found+1),grav_found(1),grav_found(2), q_total)
+         ! write(*,*) "grav_final", grav_final
+         !! equation 32 of article, in terms of singular gravity
+                                     
+         g_exterior=(q_local-q_total)/q_total
+         ! write(*,*) "grav_ext", g_exterior
+
+         if(flag_use_analyt.eq.0) then
+
+            if(flag_accel.eq.1) then
+               effective_gravity=GT_accel*(M1)*(grav_final+g_exterior)/(r_loc**2.d0)
+            else
+               effective_gravity=(grav_final+g_exterior)
+            ! write(*,*) "grav_final", effective_gravity
+
+            end if
+
+         end if 
+
+         return      
+      end function effective_gravity
+
+!!##
+!!## returns interpolated value y for x that is between x1 and x2
+!!##       
+
+       
+       real*8 function linear_interpolant(x1,x2,y1,y2,x)
+         implicit none
+         real*8 :: x1, x2, y1, y2, x
+         
+         linear_interpolant=((x-x1)*(y2-y1)/(x2-x1))+y1
+         
+       end function linear_interpolant
+
+
---
+++ gravitytable_use_example.f90
@@ -0,0 +1,62 @@
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!  example of initialization, of the use of effective gravity
+!!  and of the other useful routines
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+      program modified_grav
+        implicit none
+        
+        real*8 effective_gravity
+        real*8 grav_final_real, M1, M2, M_loc,a,r_loc, r_max, r_l1
+        integer flag_accel
+        real*8 r_linner, r_louter
+
+        integer i
+
+        ! first star in solar masses
+        M1=10.d0
+        ! second star in solar masses
+        M2=10.d0
+        ! orbital separation in solar radii
+        a=5.d0
+        ! flag_accel = 1 returns acceleration in CGS units
+        ! flag_accel = 2 returns acceleration in units of single star acceleration
+        flag_accel=2
+
+
+        ! this pre-reads tables as well as initializes L1/L2/L3 points
+        call gravity_table_ini(M1,M2,r_linner,r_louter)
+
+
+        ! r_lout is volume equivalent radius for the outer Lagrangian point
+        r_louter= r_louter*a! here add the read of the outer L point from the table
+        write(*,*) "r_lout=",r_louter,"solar radii"
+        ! r_lin is volume equivalent radius for the inner Lagrangian point
+        r_linner= r_linner! here add the read of the L1 point from the table
+
+        
+        do i=1, 25
+! this will cycle through point mass gravity at differenct distance approximately till it will fills L3           
+        ! r_loc voluem equivalent radis
+          r_loc=r_louter*(0.04d0*dble(i))
+          ! write(*,*)"example input: rloc =", r_loc
+        ! mass of the first star inside r_loc
+          ! M_loc=(M1/M2)
+           ! for experiments with the local mass ratio, here the case of a constant density star is provided. 
+           ! note that the code will not allow to use a negative density gradient.
+          ! if (r_loc.ge.r_lin) then
+          M_loc = M1*(r_loc/r_louter)**3.d0
+          ! M_loc = 0.5d0*M1
+          ! write(*,*) "M_loc=", M_loc
+          ! end if 
+          grav_final_real = effective_gravity(M1,M2,a,r_loc,M_loc,flag_accel)
+          write(*,*) "call for", i,  r_loc/a, a, grav_final_real
+        end do
+        
+        stop
+      end program modified_grav
+
+
+
+
